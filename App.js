589d7fa35208c659c5cdb2dbee8c2d9703807e32
JAZZHAND1 <rotorblade52@outlook.com>
2020-09-30 18:05:55 +0600

    #4 First navigator created and displayed

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
 
+const stack = createStackNavigator();
+
+function App(){
+  return(
+  <NavigationContainer>
+   <stack.Navigator>
+     <stack.Screen name="Home" component={HomeScreen}/>
+      </stack.Navigator>
+ </NavigationContainer>
+  );
+}
+export default App;
